32cef660b967f5d81aa7c2e681fa0edb5f2acd70
ainozerie <ainozerie@Sergeis-MacBook-Pro.local>
2022-12-09 16:42:01 +0200

    deploying login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import TelegramLoginButton from 'react-telegram-login';
+
+const handleTelegramResponse = response => {
+    console.log(response);
+};
 
 function Login() {
     return (
-        <p>Login</p>
+        <>
+            <p>{process.env.REACT_APP_MY_ENV_VARIABLE}</p>
+            <TelegramLoginButton dataOnauth={handleTelegramResponse} botName="Traveler1703Bot" />
+        </>
     );
 }
 
