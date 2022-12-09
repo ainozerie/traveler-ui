6ba4f2674dd7ecc90cafa594851c0b9ca15420f6
ainozerie <ainozerie@Sergeis-MacBook-Pro.local>
2022-12-09 13:32:05 +0200

    first commit

---
+++ src/basic/Loader.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Loader.css';
+
+
+class Button extends Component {
+    render() {
+        return <div class="loader-line"></div>;
+    }
+}
+  
+export default Button;
