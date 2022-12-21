6eaf65d5cee6ad2a364ecebb8b8810c37c1fa973
ainozerie <ainozerie@Sergeis-MacBook-Pro.local>
2022-12-21 23:35:30 +0200

    api url corrected

--- src/services/RideService.js
+++ src/services/RideService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export class RideService {
 
-    url = 'https://cors-anywhere.herokuapp.com/' + 'https://traveler-production.up.railway.app/';
+    url = 'https://traveler-production.up.railway.app/';
 
     async fetchRides(direction, afterDate, beforeDate) {
         let response = await axios.get(this.url + 'rides/' + `query?direction=${direction}&afterDate=${afterDate}&beforeDate=${beforeDate}`)
