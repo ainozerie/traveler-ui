edacbb2b675895922546d1c4da898e6628cb850f
ainozerie <ainozerie@Sergeis-MacBook-Pro.local>
2023-01-22 23:57:40 +0200

    testing getting user for profile

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Profile() {
+
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        if (localStorage.getItem('user')) {
+            setUser(JSON.parse(localStorage.getItem('user')));
+            console.log(user);
+        } else {
+            navigate('/auth');
+        }
+    }, []);
+
+
     return (
         <div className='profile'>
         <h1>Профиль</h1>
+        <p>{user.id}</p>
         </div>
     );
 }
