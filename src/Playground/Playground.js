b81a6ca11054ccee0e15e2080982027df17fcb05
ainozerie <ainozerie@Sergeis-MacBook-Pro.local>
2023-01-27 01:22:43 +0200

    playground updated

--- src/Playground/Playground.js
+++ src/Playground/Playground.js
@@ -1,35 +1,8 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { toggleIsLoading, changeUser } from '../store/session/session';
-
-
-
-// for testing matters
-
 function Playground() {
-    const isLoading = useSelector(state => state.session.isLoading)
-    const user = useSelector(state => state.session.user)
-
-    const dispatch = useDispatch();
-
-    const clicker = () => {
-        dispatch(toggleIsLoading())
-        console.log(isLoading);
-    }
-    const setUser = () => {
-        dispatch(changeUser('Valera'))
-        console.log('user is ', user);
-    }
-
-    const getUser = () => {
-        console.log('user is ', user);
-    }
+    localStorage.removeItem('user');
     return (
         <>
             <p>Playground</p>
-            <button onClick={clicker}>toggle loader</button>
-            <button onClick={setUser}>set user</button>
-            <button onClick={getUser}>get user</button>
         </>
     );
 }
