5107ac191fbe4efd35e1000d2dc1660c512e6372
borsmar <borsmar123@gmail.com>
2023-04-09 17:14:51 +0300

    heui

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -42,6 +42,8 @@ function Search() {
     const submitHandler = () => {
         rideService.fetchRides(searchFilters.direction, searchFilters.date, new Date(new Date(searchFilters.date).getTime() + 24 * 60 * 60 * 1000).toISOString().slice(0,10))
             .then((res) => {
+                console.log('hei');
+
                 console.log(res.data);
                 setRides(res.data)
             }
