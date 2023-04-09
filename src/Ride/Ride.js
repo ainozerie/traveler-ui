35d6e50224d90bc66d1a6080d5b4f1ba29f5e7ea
borsmar <borsmar123@gmail.com>
2023-04-09 23:39:28 +0300

    added number of places avaliable

--- src/Ride/Ride.js
+++ src/Ride/Ride.js
@@ -15,6 +15,7 @@ function Ride(props) {
             <div className='ride-content'>
                 <p>{props.description}</p>
             </div>
+            <div class="d-flex d-left"><a class="">Осталось мест: {props.capacity - props.currentNumberOfPassengers}</a></div>
             <div className='d-flex d-right'>
             <a className='connect'>Связаться с водителем</a>
             </div>
