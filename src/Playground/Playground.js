ec792b3002b77612d986e610e2172bb1d6c73ced
borsmar <borsmar123@gmail.com>
2023-04-10 01:38:14 +0300

    test notification

--- src/Playground/Playground.js
+++ src/Playground/Playground.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Counter from "../Filters/Counter/Counter";
+import Notification from './Notification';
+
 
 function Playground() {
     localStorage.removeItem('user');
@@ -9,11 +11,27 @@ function Playground() {
         setCapacity(count)
     }
 
+    const [rideCreated, setRideCreated] = useState(false);
+
+    const handleCreateRide = () => {
+      // Code to create ride goes here
+  
+      setRideCreated(true);
+  
+      setTimeout(() => {
+        setRideCreated(false);
+      }, 3000);
+    };
+
+
     return (
         <>
             <p>Playground</p>
             <Counter getCount={getCount}
                 count={capacity} min='1' max='8'  />
+                 <button onClick={handleCreateRide}>Create Ride</button>
+
+            {rideCreated && <Notification message="Ride Created" />}
         </>
     );
 }
