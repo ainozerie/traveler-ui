8ee75c6f7f53e2075b7bbc9bbb65fd0ce7bd4ddf
borsmar <borsmar123@gmail.com>
2023-04-16 19:46:15 +0300

    added iphone app

--- src/Playground/Playground.js
+++ src/Playground/Playground.js
@@ -9,9 +9,36 @@ function Playground() {
         setCapacity(count)
     }
     // hi, Borya!
+
+    const handleIphone = () => {
+      // Проверяем, поддерживает ли браузер Web App Manifest API
+  if ('standalone' in window.navigator && window.navigator.standalone) {
+  // Браузер уже добавил приложение на рабочий стол
+  } else if (window.matchMedia('(display-mode: standalone)').matches) {
+  // Приложение уже открыто в полноэкранном режиме
+  } else {
+  // Создаем кнопку "Добавить на рабочий стол"
+  var addToHomeScreenButton = document.createElement('button');
+  addToHomeScreenButton.innerText = 'Добавить на рабочий стол';
+  addToHomeScreenButton.addEventListener('click', function() {
+    // Добавляем метку приложения на рабочий стол
+    window.navigator.standalone = true;
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'manifest');
+    link.setAttribute('href', '/manifest.json');
+    document.head.appendChild(link);
+  });
+  document.body.appendChild(addToHomeScreenButton);
+  }
+
+};
+
+
+
     return (
         <>
             <p>Playground</p>
+            <button onClick={handleIphone}>Handle phone app</button>
             <Counter getCount={getCount}
                 count={capacity} min='1' max='8'  />
         </>
