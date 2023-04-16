2efcc4ca14d29f1e4e3350473525efa8f431ed9d
Ainozerov Sergei <ainozerie.prod@gmail.com>
2023-04-16 19:08:52 +0300

    main after merging

--- src/Playground/Playground.js
+++ src/Playground/Playground.js
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import Counter from "../Filters/Counter/Counter";
-import addToHomescreen from "add-to-homescreen";
-import AddToHomeScreen from '@ideasio/add-to-homescreen-react';
 
 
 function Playground() {
@@ -24,28 +22,11 @@ function Playground() {
       }, 3000);
     };
 
-    const handleIphone = () => {
-      alert(navigator.userAgent.match(/iPhone/i) && !navigator.standalone)
-        // Проверяем, работает ли браузер на iOS и не находится ли приложение уже на главном экране
-        if (navigator.userAgent.match(/iPhone/i) && !navigator.standalone) {
-          var addToHomeConfig = {
-            autostart: false,
-            message: 'Добавить на главный экран?',
-            touchIcon: 'https://png.pngtree.com/element_our/20190530/ourmid/pngtree-correct-icon-image_1267804.jpg',
-            arrow: true,
-            lifespan: 0
-          };
-          var addToHome = addToHomescreen(addToHomeConfig);
-          addToHome.show();
-        
-        }
-        
-      };
+    const handleIphone = () => {};
 
     return (
         <>
             <p>Playground</p>
-            <AddToHomeScreen />
             <Counter getCount={getCount}
                 count={capacity} min='1' max='8'  />
                  <button onClick={handleCreateRide}>Create Ride</button>
--- src/Ride/Ride.js
+++ src/Ride/Ride.js
@@ -9,10 +9,10 @@ function Ride(props) {
                     <img src='https://www.pngall.com/wp-content/uploads/12/Driver-PNG-HD-Image.png' />
                     <p className='username'>{props.firstname + ' ' + props.surname}</p>
                 </div>
-                <p className='price'>{ride.price} euro</p>
+                <p className='price'>{props.price} euro</p>
             </div>
             <div className='ride-content'>
-                <p>{ride.description}</p>
+                <p>{props.description}</p>
             </div>
             <div className="d-flex d-left"><a className="">Осталось мест: {props.numberOfPlacesAvailable}</a></div>
             <div className='d-flex d-right'>
